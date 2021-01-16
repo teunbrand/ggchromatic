49287423d3812746808c65ec9931e563610b93e7
teunbrand <tahvdbrand@gmail.com>
2021-01-16 22:04:40 +0100

    Init skeleton

---
+++ R/ggchromatic-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+#' @import scales
+#' @import rlang
+#' @import ggplot2
+#' @import vctrs
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
